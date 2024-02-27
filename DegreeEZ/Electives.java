35db7b6d4349f5f97a500df5e38a7a5e69023a34
anshpatel14 <anshjun14@gmail.com>
2024-02-27 12:44:13 -0500

    Creating Electives Class

---
+++ DegreeEZ/Electives.java
@@ -0,0 +1,15 @@
+package DegreeEZ;
+import java.util.ArrayList;
+
+public class Electives  {
+    private int creditsRequired;
+    private ArrayList<Course> courseOptions;
+
+    public ArrayList<Course> getLibArts() {
+        // TODO
+    }
+
+    public ArrayList<Course> getElectives() {
+        // TODO
+    }    
+}
