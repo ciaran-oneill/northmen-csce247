f66dfd3c39a7bcd905d3514a89657a3d5fa5195e
tyler logue <tylerlogue3@gmail.com>
2024-02-27 12:29:46 -0500

    CompletedCourse Stub

---
+++ DegreeEZ/Student.java
@@ -0,0 +1,23 @@
+package DegreeEZ;
+
+import java.util.ArrayList;
+
+public class Student {
+    private Major major;
+    private ArrayList<CompletedCourse> completedCourses;
+    private ArrayList<Course> enrolledClasses;
+    private ArrayList<Course> outstandingReq;
+
+
+    public Student (String firstName, String lastName, String password, String email, Major major) {
+        //TODO
+    }
+
+    public String viewDegreeProgress() {
+        //TODO
+    }
+
+    public void requestAdvisingAppointment() {
+        //TODO
+    }
+}
