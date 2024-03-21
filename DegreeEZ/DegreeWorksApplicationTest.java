88eb47e9a99346924f1f6165090808b6a9ce6d7e
tyler logue <tylerlogue3@gmail.com>
2024-03-21 12:38:39 -0400

    new Tests

---
+++ DegreeEZ/DegreeWorksApplicationTest.java
@@ -0,0 +1,13 @@
+package DegreeEZ;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.ArrayList;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class DegreeWorksApplicationTest {
+    
+}
