c554e417e2e688950af1adfd3c504876a8fd81e6
anshpatel14 <anshjun14@gmail.com>
2024-03-24 22:08:24 -0400

    Renaming from CourseTest.java to CourseListTest.java

--- DegreeEZ/CourseListTest.java
+++ DegreeEZ/CourseListTest.java
@@ -19,7 +19,7 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-public class CourseTest {
+public class CourseListTest {
     private Course course;
 
     @BeforeEach
